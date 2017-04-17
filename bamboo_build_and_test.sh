2db4a71e860ee1caf12dfd333d20da23e489a221
Christopher Dunn <cdunn@pacificbiosciences.com>
2017-04-17 16:50:58 -0700

    Copied from bamboo

---
+++ bamboo_build_and_test.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#rm -rf FALCON-integrate
+
+#git clone https://github.com/PacificBiosciences/FALCON-integrate
+#cd FALCON-integrate
+pwd
+ls -l
+git submodule
+git --version
+type module >& /dev/null || . /mnt/software/Modules/current/init/bash
+module unload git gcc ccache
+module load git/2.8.3
+module load gcc/4.9.2
+module load ccache/3.2.3
+#module load make
+
+set -vex
+git --version
+which gcc
+which g++
+gcc --version
+# We cannot use /bin/python without /bin/gcc.
+export PATH=/mnt/software/a/anaconda2/4.2.0/bin:$PATH
+which python
+
+export CCACHE_DIR=/mnt/secondary/Share/tmp/bamboo.mobs.ccachedir
+
+git remote -v
+ls -larth
+pwd
+#git submodule update --init #No! We must use BB for some,
+# and rel URLs do not work for file://nothing
+MY_BRANCH=${bamboo.planRepository.1.branch}
+#git submodule foreach git pull origin ${MY_BRANCH}
+git submodule foreach git checkout ${MY_BRANCH}
+#git submodule update --init git-sym FALCON-make FALCON-examples
+git submodule
+./travis.sh
+ls -l $HOME/.ccache
+cat $HOME/.ccache/ccache.conf
+date --utc
